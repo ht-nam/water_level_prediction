54aacb71d85974dd5cc8bc50f32a5a3d90e9edb9
ht-nam <hoangthanhnam10@gmail.com>
2023-05-19 21:36:46 +0700

    Reformat code

--- lstmModel.py
+++ lstmModel.py
@@ -6,7 +6,7 @@ from exportData import printResult
 
 trainFile = "dataset\Train_data_WL_RF_21_22.csv"
 testFile = "dataset\Test_data_WL_RF_21_22.csv"
-cols = [
+knowCols = [
     "WL_KienGiang",
     "RF_KienGiang",
     "WL_LeThuy",
@@ -15,26 +15,37 @@ cols = [
     "RF_DongHoi",
     "Tide_DongHoi",
 ]
-is_smote = True
-label_col = "WL_LeThuy"
-step_days = 6
-callback_days = 6
+labelCol = "WL_LeThuy"
+callbackTime = 6
+stepTime = 6
 epochs = 10
-batch_size = 30
-water_level = 1.2
+batchSize = 30
+waterLevel = 1.2
+isSmote = True
 
 
-def lstmModel():
+def lstmModel(
+    trainFile,
+    testFile,
+    knowCols,
+    labelCol,
+    callbackTime=6,
+    stepTime=6,
+    epochs=100,
+    batchSize=30,
+    waterLevel=1,
+    isSmote=False,
+):
     # get data
     x_train, y_train, x_test, y_test = loadData(
         trainFile=trainFile,
         testFile=testFile,
-        cols=cols,
-        label_col=label_col,
-        step_days=step_days,
-        callback_days=callback_days,
-        water_level=water_level,
-        is_smote=is_smote,
+        cols=knowCols,
+        label_col=labelCol,
+        step_days=stepTime,
+        callback_days=callbackTime,
+        water_level=waterLevel,
+        is_smote=isSmote,
     )
 
     # train model
@@ -55,7 +66,7 @@ def lstmModel():
         x_train,
         y_train,
         epochs=epochs,
-        batch_size=batch_size,
+        batch_size=batchSize,
         validation_split=0.3,
         verbose=1,
     )
@@ -65,7 +76,18 @@ def lstmModel():
     y_prd = model.predict(x_test)
     y_test_inverse = y_scaler.inverse_transform(y_test)
     y_prd_inverse = y_scaler.inverse_transform(y_prd)
-    printResult(y_test_inverse, y_prd_inverse, [], callback_days)
+    printResult(y_test_inverse, y_prd_inverse, [], callbackTime)
 
 
-lstmModel()
+lstmModel(
+    trainFile,
+    testFile,
+    knowCols,
+    labelCol,
+    callbackTime,
+    stepTime,
+    epochs,
+    batchSize,
+    waterLevel,
+    isSmote,
+)
